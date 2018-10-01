adc91d5e352d40cf484a12638165fc5c08a28537
Christian <christianmonte@gmail.com>
2018-09-30 22:49:43 -0500

    all

---
+++ README.md
@@ -0,0 +1,29 @@
+## CMO Scroll Animation
+
+Basic JS scroll animation library.
+
+
+### Install
+```
+npm i cmoscrollanimation
+```
+
+### Use
+
+```javascript
+import cmoScrollAnimation from 'cmoScrollAnimation';
+```
+
+```javascript
+  cmoScrollAnimation(); 
+```
+
+```html
+  <section class="animation">
+    <p>Lorem ipsum.</p>
+  </section>
+```
+
+### Demo
+
+For more details see [the proyect website](https://christianmo.github.io/cmoScrollAnimation/).
---
+++ src/cmoScrollAnimation.js
@@ -0,0 +1,39 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.cmoHashNavigation = factory();
+  }
+}(this, function() {
+  'use strict';
+
+  var cmoScrollAnimation = function animation() {
+    var withAnimation = document.querySelectorAll('.animation');
+
+    if (!withAnimation) return false;
+  
+    window.addEventListener('scroll', function () {
+      [].forEach.call(withAnimation, function (el) {
+        var currentTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
+        var animationPos = currentTop + window.innerHeight / 2;
+        var elTopPos = el.offsetTop;
+        var elBottomPos = elTopPos + el.offsetHeight;
+  
+        if (animationPos > elTopPos) {
+          el.classList.add('animated');
+        }
+  
+        if (el.classList.contains('animation--focused')) {
+          if (animationPos > elBottomPos || animationPos < elTopPos) {
+            el.classList.remove('animated');
+          }
+        }
+      });
+    });
+  };
+
+  return cmoScrollAnimation;
+}))
